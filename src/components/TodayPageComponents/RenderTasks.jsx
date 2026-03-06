959bf0b7b86bbc749e0c705ddf48f50a9d221f78
bhushanpatil288 <sharewithbhushan@gmail.com>
2026-03-06 23:51:22 +0530

    [add] delete funcationality

--- src/components/TodayPageComponents/RenderTasks.jsx
+++ src/components/TodayPageComponents/RenderTasks.jsx
@@ -1,20 +1,27 @@
-const RenderTasks = ({tasks, setTasks}) => {
+import { FaTrash } from "react-icons/fa6";
+import { useContext } from "react";
+import { TaskContext } from "../../context/Tasks";
 
-  const toggleTaskStatus = (taskId) =>{
-    let taskIndex = tasks.findIndex(task=>task.id === taskId);
-    let newTasks = [...tasks];
-    newTasks[taskIndex].complete = !newTasks[taskIndex].complete
-    setTasks(newTasks)
-  }
+const RenderTasks = ({ tasks, setTasks }) => {
+
+  const { toggleTaskStatus, deleteTask } = useContext(TaskContext)
 
   return (
     <ul>
       {
         tasks.map(task => {
           return (
-            <li key={task.id} className="flex gap-3 mx-3 px-2 py-3 hover:bg-blue-100 rounded border-b border-gray-200">
-              <input type="checkbox" id={task.id + "task"} onChange={()=>{toggleTaskStatus(task.id)}} />
-              <label htmlFor={task.id + "task"} className={`cursor-pointer ${task.complete ? "line-through": ""}`}>{task.taskTitle}</label>
+            <li key={task.id} className="flex justify-between mx-3 px-2 py-3 hover:bg-blue-100 rounded border-b border-gray-200">
+              <div className="flex gap-3 items-center">
+                <input type="checkbox" id={task.id + "task"} onChange={() => { toggleTaskStatus(task.id) }} />
+                <label htmlFor={task.id + "task"} className={`cursor-pointer ${task.complete ? "line-through" : ""}`}>{task.taskTitle}</label>
+              </div>
+              <button 
+                onClick={()=> deleteTask(task.id)}
+                className="p-2 bg-linear-to-br from-red-400 to-red-500 rounded-md px-2 text-white shadow cursor-pointer hover:bg-linear-to-br hover:from-red-400 hover:to-red-600 transition-all transition-duration-300"
+              >
+                <FaTrash />
+              </button>
             </li>
           )
         })
--- src/context/Tasks.jsx
+++ src/context/Tasks.jsx
@@ -4,8 +4,23 @@ export const TaskContext = createContext(null);
 
 const Tasks = ({children}) => {
   const [tasks, setTasks] = useState([{id: 1, taskTitle: "testing", complete:false, tag:null}, {id: 2, taskTitle: "testing2", complete:false, tag:null}])
+
+  const toggleTaskStatus = (taskId) => {
+    let taskIndex = tasks.findIndex(task => task.id === taskId);
+    let newTasks = [...tasks];
+    newTasks[taskIndex].complete = !newTasks[taskIndex].complete
+    setTasks(newTasks)
+  }
+
+  const deleteTask = (taskId) =>{
+    let taskIndex = tasks.findIndex(task=> task.id === taskId);
+    let newTasks = [...tasks];
+    newTasks.splice(taskIndex, 1);
+    setTasks(newTasks);
+  }
+
   return (
-    <TaskContext value={{tasks, setTasks}}>
+    <TaskContext value={{tasks, setTasks, toggleTaskStatus, deleteTask}}>
       {children}
     </TaskContext>
   )
